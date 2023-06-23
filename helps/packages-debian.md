364ad91c2fa290cd97db4ab852df0728199e926e
marcos.silvestrini <marcos.silvestrini@gmail.com>
2023-06-23 18:24:43 -0300

    Update files

---
+++ helps/packages-debian.md
@@ -0,0 +1,11 @@
+# Install commons  packages
+
+## Network
+
+```sh
+# ping
+apt install iputils-ping -y
+
+#ip
+apt install iproute2 -y
+```
--- scripts/linux/docker/userdata-docker.sh
+++ scripts/linux/docker/userdata-docker.sh
@@ -29,27 +29,8 @@ fi
 # Uninstall old versions
 for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do apt-get remove $pkg; done
 
-# Add Docker’s official GPG key:
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --yes --dearmor -o /etc/apt/keyrings/docker.gpg
-chmod a+r /etc/apt/keyrings/docker.gpg
-
-# Use the following command to set up the repository:
-echo \
-"deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-tee /etc/apt/sources.list.d/docker.list > /dev/null
-
 # Install Docker Engine
-
-## Update the apt package index:
-apt-get update -y >/dev/null
-
-## Install pre reqs
-apt-get install ca-certificates curl gnupg
-
-## Install Docker Engine, containerd, and Docker Compose.
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y >/dev/null
+curl -fsSL https://get.docker.com/ | sh
 
 ## Check installation
 docker --version
@@ -57,3 +38,6 @@ docker --version
 ## install docker compose
 apt install -y docker-compose
 apt autoremove -y
+
+## set permissions
+usermod -aG docker vagrant
